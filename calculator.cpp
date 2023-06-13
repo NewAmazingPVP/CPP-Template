aee5a4b48c664bb92e4c6a0ec31dae026c1d2006
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-06-13 13:31:35 -0400

    Initial commit

---
+++ calculator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    double x = M_PI / 2.0;
+    double result = sin(x);
+    std::cout << "The sine of " << x << " is " << result << std::endl;
+    std::cout << "The end!" << std::endl;
+    return 0;
+}
